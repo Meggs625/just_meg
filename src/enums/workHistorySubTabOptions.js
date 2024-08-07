a644b6cc6a2d0f1b4e8c3810e6c1e490d47dcf4d
Megan McBride <megan.mcbride@colorado.edu>
2024-08-07 15:21:57 -0600

    Change formatting of work history

---
+++ src/enums/workHistorySubTabOptions.js
@@ -0,0 +1,8 @@
+const WORK_HISTORY_SUBTAB_OPTIONS = {
+    COMPANY: "Company",
+    MAIN_PROJECT: "Main Project",
+    EXPERIENCE: "Experience",
+    MORE_DETAILS: "More Details"
+}
+
+export default WORK_HISTORY_SUBTAB_OPTIONS;
--- src/views/WorkHistory/WorkHistory.jsx
+++ src/views/WorkHistory/WorkHistory.jsx
@@ -1,33 +1,76 @@
-import React, { useState } from 'react';
-import styles from './WorkHistory.module.scss';
-import workHistoryBtn from '../../dataFiles/workHistoryDetails';
-import Button from '../../components/Button/Button';
-import Title from '../../components/Title/Title';
-import MainDetailsCard from '../../components/MainDetailsCard/MainDetailsCard';
-import MainProjectCard from '../../components/MainProjectCard/MainProjectCard';
-import MoreCompanyDetails from '../../components/MoreCompanyDetailsCard/MoreCompanyDetailsCard';
-import MoreExperienceCard from '../../components/MoreExperienceCard/MoreExperienceCard';
+import React, { useState } from "react";
+import styles from "./WorkHistory.module.scss";
+import workHistoryBtn from "../../dataFiles/workHistoryDetails";
+import Button from "../../components/Button/Button";
+import Title from "../../components/Title/Title";
+import MainDetailsCard from "../../components/MainDetailsCard/MainDetailsCard";
+import MainProjectCard from "../../components/MainProjectCard/MainProjectCard";
+import MoreCompanyDetails from "../../components/MoreCompanyDetailsCard/MoreCompanyDetailsCard";
+import MoreExperienceCard from "../../components/MoreExperienceCard/MoreExperienceCard";
+import WORK_HISTORY_SUBTAB_OPTIONS from "../../enums/workHistorySubTabOptions";
 
 export default function WorkHistory() {
+  const [selectedCompanyDetails, setSelectedCompanyDetails] = useState(null);
+  const [currentTab, setCurrentTab] = useState(
+    WORK_HISTORY_SUBTAB_OPTIONS.COMPANY
+  );
+  console.log(currentTab, "current");
 
-    const [selectedCompanyDetails, setSelectedCompanyDetails] = useState(null);
+  return (
+    <section className={styles.work_history_wrapper}>
+      <Title variant="1">Work History</Title>
+      <section className={styles.btn_container}>
+        {workHistoryBtn.map((history) => (
+          <Button
+            key={history.id}
+            onClick={() => setSelectedCompanyDetails(history)}
+            variant="company_logos"
+          >
+            <img src={history.img} className={styles.logo_img} />
+          </Button>
+        ))}
+      </section>
 
-    return (<section className={styles.work_history_wrapper}>
-        <Title variant="1">Work History</Title>
-        <section className={styles.btn_container}> 
-            {workHistoryBtn.map((history) => (
-                <Button key={history.id} onClick={() => setSelectedCompanyDetails(history)} variant="company_logos"><img src={history.img} className={styles.logo_img}/></Button>
-            ))}
-        </section>
-            {selectedCompanyDetails && (
-                <section className={styles.detail_container}> 
-                    <MainDetailsCard mainDetails={selectedCompanyDetails?.mainDetails}/>
-                    <MainProjectCard projectDetails={selectedCompanyDetails?.mainProject} />
-                    <MoreCompanyDetails otherDetails={selectedCompanyDetails?.otherDetails} />
-                    <MoreExperienceCard otherExperience={selectedCompanyDetails?.otherExperiences} />
-
-
-                </section>
+      {selectedCompanyDetails && (
+        <seciont className={styles.details_container}>
+          <section>
+            {Object.entries(WORK_HISTORY_SUBTAB_OPTIONS).map(([key, label]) => {
+              return (
+                <button
+                  key={key}
+                  onClick={() =>
+                    setCurrentTab(WORK_HISTORY_SUBTAB_OPTIONS[key])
+                  }
+                >
+                  {label}
+                </button>
+              );
+            })}
+          </section>
+          <section>
+            {currentTab === WORK_HISTORY_SUBTAB_OPTIONS.COMPANY && (
+              <MoreCompanyDetails
+                otherDetails={selectedCompanyDetails?.otherDetails}
+              />
             )}
-    </section>)
+            {currentTab === WORK_HISTORY_SUBTAB_OPTIONS.MAIN_PROJECT && (
+              <MainProjectCard
+                projectDetails={selectedCompanyDetails?.mainProject}
+              />
+            )}
+            {currentTab === WORK_HISTORY_SUBTAB_OPTIONS.EXPERIENCE && (
+              <MoreExperienceCard
+                otherExperience={selectedCompanyDetails?.otherExperiences}
+              />
+            )}
+            {currentTab === WORK_HISTORY_SUBTAB_OPTIONS.MORE_DETAILS && (
+              <MainDetailsCard
+                mainDetails={selectedCompanyDetails?.mainDetails}
+              />
+            )}
+          </section>
+        </seciont>
+      )}
+    </section>
+  );
 }
