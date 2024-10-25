b48a1bad96f72347763464d125b1573853ed6bc3
Megan McBride <megan.mcbride@colorado.edu>
2024-10-24 19:13:41 -0600

    Align slide headers in the center of the screen

--- src/views/AboutMe/SlideContainer.jsx
+++ src/views/AboutMe/SlideContainer.jsx
@@ -5,13 +5,20 @@ export default function SlideContainer({ slideDetails }) {
   const { title, imgSrc, alt, description, link, linkText } = slideDetails;
   return (
     <section className={styles.slide_wrapper}>
-      <h1>{title}</h1>
+      <h1 className={styles.slide_header}>{title}</h1>
       <div className={styles.detail_container}>
         <div className={styles.img_container}>
           <img src={imgSrc} alt={alt} />
         </div>
         <div className={styles.description_container}>
-          <p>{description} {link && <a href={link} target="_blank">{linkText}.</a>}</p>           
+          <p>
+            {description}{" "}
+            {link && (
+              <a href={link} target="_blank">
+                {linkText}.
+              </a>
+            )}
+          </p>
         </div>
       </div>
     </section>
