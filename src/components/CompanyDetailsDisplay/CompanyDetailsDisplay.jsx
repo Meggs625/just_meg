d0aa408388038126b6a8f776bb5c2bc23910a618
Megan McBride <megan.mcbride@colorado.edu>
2024-10-29 13:43:07 -0600

    WIP Building out company details

--- src/components/CompanyDetailsDisplay/CompanyDetailsDisplay.jsx
+++ src/components/CompanyDetailsDisplay/CompanyDetailsDisplay.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useMemo } from "react";
+import styles from "./CompanyDetailsDisplay.module.scss";
 
 export default function CompanyDetailsDisplay({
   selectedCompanyDetails,
   clearSelectedCompanyDetails,
 }) {
+  const { mainDetails, otherDetails, otherExperiences, mainProject } =
+    selectedCompanyDetails;
+
+  const formattedTechStack = useMemo(() => {
+    if (mainDetails.techStack.length) {
+      return mainDetails.techStack.join(", ");
+    }
+  }, [selectedCompanyDetails]);
+
+  const quickDetails = {};
   return (
-    <div>
-      <h1>Hello!</h1>
-      <button onClick={() => clearSelectedCompanyDetails()}>X</button>
-    </div>
+    <section className={styles.company_details_wrapper}>
+      <header className={styles.company_details_header}>
+        <h1 className={styles.company_title}>{mainDetails.company}</h1>
+        <button onClick={() => clearSelectedCompanyDetails()}>X</button>
+      </header>
+      <section>
+        <section>
+          <p>{`${mainDetails.startDate} - ${mainDetails.endDate}`}</p>
+          <p>{mainDetails.jobTitle}</p>
+          <p>{otherDetails.workEnvironment}</p>
+          <p>{otherDetails.tickets}</p>
+          <p>{formattedTechStack}</p>
+        </section>
+        <section>
+          {otherExperiences.map((experience, idx) => (
+            <p key={idx}>{`* ${experience}`}</p>
+          ))}
+        </section>
+        <section>
+          <p>{mainProject}</p>
+        </section>
+      </section>
+    </section>
   );
 }
