8504c43ed6be9f888df613070b3eac01aff3b570
Megan McBride <megan.mcbride@colorado.edu>
2024-11-01 17:01:23 -0600

    Establish reusable project display card

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -1,44 +1,18 @@
-import React from 'react';
-import styles from './Projects.module.scss';
-import {motion} from "framer-motion";
-import Title from '../../components/Title/Title';
+import React from "react";
+import styles from "./Projects.module.scss";
+import Title from "../../components/Title/Title";
+import projectDetails from "../../dataFiles/projectDetails";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
 
 export default function Projects() {
-    const sentence = {
-        hidden: {opacity: 1},
-        visible: {
-            opacity: 1, 
-            transition: {
-                delay: 0.5,
-                staggerChildren: 0.08,
-                repeat: Infinity, 
-                duration: 2
-            },
-        },
-    };
-
-    const letter = {
-        hidden: {opacity: 0, y: 50},
-        visible: {
-            opacity: 1,
-            y: 0,
-            transition: {
-            },
-        },
-    }
-    return (
+  return (
     <section className={styles.projects_wrapper}>
-        <Title variant="1">My Projects</Title>
-        <div className={styles.project_description_container}>
-            <motion.h3 variants={sentence} initial="hidden" animate="visible">
-                {"Coming Soon!".split("").map((char, index) => {
-                return (
-                    <motion.span key={`${char} ${index}`} variants={letter}>{char}</motion.span>
-                    )}
-                    )}
-            </motion.h3>
-        </div>
-
+      <Title variant="1">My Projects</Title>
+      <div className={styles.project_description_container}>
+        {projectDetails.map(({ title, img, alt, link }) => (
+          <ProjectCard title={title} img={img} alt={alt} link={link} />
+        ))}
+      </div>
     </section>
-)
+  );
 }
