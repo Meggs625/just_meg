216637f6e27c6c674b8d4d5a5e3a2ccbaa6f366f
Megan McBride <megan.mcbride@colorado.edu>
2024-11-02 14:55:10 -0600

    Add contact information footer

--- src/views/Welcome/Welcome.jsx
+++ src/views/Welcome/Welcome.jsx
@@ -3,42 +3,46 @@ import { motion } from "framer-motion";
 import styles from "./Welcome.module.scss";
 import purpleFlower from "../../assets/images/purpleFlower.png";
 import { ReactTyped } from "react-typed";
+import Footer from "../../components/Footer/Footer";
 
 export default function Welcome() {
   return (
     <section className={styles.welcome_container}>
-      <motion.div
-        className={styles.flower_container}
-        animate={{ rotate: 30 }}
-        transition={{
-          duration: 5,
-          repeat: Infinity,
-          repeatType: "reverse",
-        }}
-      >
-        <img
-          src={purpleFlower}
-          alt="purple-flower"
-          className={styles.background_flower}
-        />
-      </motion.div>
-      <div className={styles.descriptive_container}>
-        <h1>HELLO!</h1>
-        <h2>I'm Meg,</h2>
-        <ReactTyped
-          typeSpeed={50}
-          backSpeed={50}
-          strings={[
-            "passionate developer",
-            "creative mind",
-            "avid learner",
-            "bibliophile",
-            "recovering perfectionist",
-          ]}
-          loop={true}
-          style={{ fontSize: "1.5em" }}
-        />
+      <div className={styles.main_welcome_content}>
+        <div
+          className={styles.flower_container}
+          animate={{ rotate: 30 }}
+          transition={{
+            duration: 5,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        >
+          <img
+            src={purpleFlower}
+            alt="purple-flower"
+            className={styles.background_flower}
+          />
+        </div>
+        <div className={styles.descriptive_container}>
+          <h1>HELLO!</h1>
+          <h2>I'm Meg,</h2>
+          <ReactTyped
+            typeSpeed={50}
+            backSpeed={50}
+            strings={[
+              "passionate developer",
+              "creative mind",
+              "avid learner",
+              "bibliophile",
+              "recovering perfectionist",
+            ]}
+            loop={true}
+            style={{ fontSize: "1.5em" }}
+          />
+        </div>
       </div>
+      <Footer />
     </section>
   );
 }
