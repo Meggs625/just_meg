8e645c8824560c4b5bbdb53cf9a98b62a70e9b14
Megan McBride <megan.mcbride@colorado.edu>
2024-11-13 22:06:35 -0700

    Animate the light/dark toggle

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { motion } from "framer-motion";
 import styles from "./ThemeToggle.module.scss";
 import sun from "../../assets/images/sun.svg";
 import moon from "../../assets/images/moon.png";
@@ -12,11 +13,27 @@ export default function ThemeToggle() {
   };
 
   return (
-    <button className={styles.theme_btn} onClick={changeTheme}>
+    <motion.button
+      className={styles.theme_btn}
+      onClick={changeTheme}
+      key={`${theme} image`}
+      initial={{
+        opacity: 1,
+        y: 25,
+      }}
+      animate={{
+        opactiy: 0,
+        y: 0,
+      }}
+      exit={{
+        opactiy: 0,
+        y: 0,
+      }}
+    >
       <img
         src={theme === "light" ? moon : sun}
         alt={theme === "light" ? "dark mode" : "light mode"}
       />
-    </button>
+    </motion.button>
   );
 }
