d190e2d6eaa3d090621329f2e87e8211fb28c221
Megan McBride <megan.mcbride@colorado.edu>
2025-02-24 10:25:14 -0700

    Initial profile pic add WIP

--- src/views/Welcome/Welcome.jsx
+++ src/views/Welcome/Welcome.jsx
@@ -5,18 +5,24 @@ import pinkFlower from "../../assets/images/pinkFlower.svg";
 import { ReactTyped } from "react-typed";
 import Footer from "../../components/Footer/Footer";
 import { ThemeContext } from "../../context/ThemeProvider";
+import mePic from "../../assets/images/jacadranda_pic.jpg"
 
 export default function Welcome() {
   const { theme } = useContext(ThemeContext);
   return (
     <section className={styles.welcome_container}>
       <div className={styles.main_welcome_content}>
-        <div className={styles.flower_container}>
-          <img
-            src={theme === "light" ? purpleFlower : pinkFlower}
-            alt="flower"
-            className={styles.background_flower}
-          />
+        <div className={styles.pic_flower_container}>
+          <div className={styles.pic_container}>
+            <img src={mePic}/>
+          </div>
+          <div className={styles.flower_container}>
+            <img
+              src={theme === "light" ? purpleFlower : pinkFlower}
+              alt="flower"
+              className={styles.background_flower}
+            />
+          </div>
         </div>
         <div className={styles.descriptive_container}>
           <h1>HELLO!</h1>
@@ -29,7 +35,6 @@ export default function Welcome() {
               "creative mind",
               "avid learner",
               "bibliophile",
-              "recovering perfectionist",
             ]}
             loop={true}
             style={{ fontSize: "1.5em" }}
